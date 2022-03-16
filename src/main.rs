7db036cd22062286a240ca868053ae3a4d63bf20
Alexandru Olaru <alxolr@gmail.com>
2022-03-16 16:08:04 +0200

    Added the generation of the task

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok(); // load envars
     let api_key = env::var("SPIRA_API_KEY")?;
     let username = env::var("SPIRA_USERNAME")?;
-    let base_url = env::var("SPIRA_URL")?;
+    let base_url = env::var("SPIRA_API_URL")?;
 
     let spira_client = SpiraClient::new(&base_url, &api_key, &username)?;
     let tasks = get_yaml_tasks();
 
-    let users = spira_client.user.list(33).await?;
-
-
-    // let task = spira_client.task.create(33, task).await?;
-    // let task = spira_client.task.get(33, 31005).await?;
-
-    println!("{:#?}", users);
+    for task in tasks {
+        let task = spira_client.task.create(task.project_id, task).await?;
+        println!("{}\n{}\n", task.name, task.get_link());
+    }
 
     Ok(())
 }
@@ -30,8 +27,7 @@ fn get_yaml_tasks() -> Vec<TaskDto> {
     let path = Path::new(&arg);
     let input = std::fs::File::open(path).expect("Provided file does not exist");
     let rdr = BufReader::new(input);
-    let tasks: Vec<TaskDto> =
-        serde_yaml::from_reader(rdr).expect("Could not process the tasks.yaml file");
+    let result = serde_yaml::from_reader(rdr).expect("Could not parse the yaml file");
 
-    tasks
+    result
 }
--- src/spira_client/entities/task.rs
+++ src/spira_client/entities/task.rs
@@ -1,20 +1,47 @@
-use reqwest::{header::HeaderValue, Client};
-use serde::{Deserialize, Serialize};
+use std::env;
 
 use crate::spira_client::Response;
+use reqwest::{header::HeaderValue, Client};
+use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
 
+#[skip_serializing_none]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TaskDto {
+    #[serde(rename = "TaskId")]
+    pub task_id: Option<u64>,
+
+    #[serde(rename = "ProjectId")]
+    pub project_id: u64,
+
     #[serde(rename = "TaskStatusId")]
     pub task_status_id: u64,
+
+    #[serde(rename = "TaskTypeId")]
+    pub task_type_id: u64,
+
     #[serde(rename = "RequirementId")]
     pub requirement_id: u64,
+
     #[serde(rename = "Name")]
     pub name: String,
+
     #[serde(rename = "OwnerId")]
     pub owner_id: u64,
+
     #[serde(rename = "EstimatedEffort")]
     pub estimated_effort: u64,
+}
+
+impl TaskDto {
+    pub fn get_link(&self) -> String {
+        format!(
+            "{}/{}/Task/{}.aspx",
+            env::var("SPIRA_BASE_URL").unwrap(),
+            self.project_id,
+            self.task_id.unwrap()
+        )
+    }
 }
 
 pub struct TaskClient<'a> {
