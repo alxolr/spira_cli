e02d0d9dca8729aa25b404c426711afd8fdeed9f
Alexandru Olaru <alxolr@gmail.com>
2022-12-09 11:49:56 +0200

    Added business value in my command

--- src/resources/incident/list_my.rs
+++ src/resources/incident/list_my.rs
@@ -1,3 +1,4 @@
+use serde_json::Number;
 use spira::SpiraClient;
 use std::error::Error;
 use structopt::StructOpt;
@@ -12,9 +13,28 @@ impl ListMy {
     pub async fn run(&self, client: &SpiraClient<'_>) -> Result<(), Box<dyn Error>> {
         let incidents = client.incident.list_my().await?;
 
-        incidents
-            .iter()
-            .for_each(|incident| println!("{} - {}", incident.get_link(), incident.name));
+        incidents.iter().for_each(|incident| {
+            let custom_properties = incident.custom_properties.clone().unwrap();
+
+            let item = custom_properties.iter().find(|property| {
+                let definition = property.get("Definition").unwrap();
+                let name = definition.get("Name").unwrap().to_string();
+
+                name.contains("Business Value")
+            });
+
+            if item.is_some() {
+                let business_value = item.unwrap().get("IntegerValue").unwrap();
+                println!(
+                    "{} {} {}",
+                    incident.get_link(),
+                    business_value,
+                    incident.name
+                )
+            } else {
+                println!("{} {}", incident.get_link(), incident.name)
+            }
+        });
 
         Ok(())
     }
